2024d466d8804b1b44be2d4a16d91a35cf9465fe
chtenb <ctenbrinke@gmail.com>
2022-06-06 15:56:54 +0200

    MVP

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
+#[macro_use]
+extern crate lazy_static;
+
 extern crate anyhow;
+
+use std::env;
 
 use anyhow::Result;
 use clap::Parser;
+use cpal::{
+    traits::{DeviceTrait, HostTrait},
+    Device,
+};
+extern crate cpal;
 
 mod record;
 
@@ -9,38 +19,86 @@ mod record;
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
-    /// Optionally specify a different working directory than the current one
+    /// Optionally specify a different output directory than the current working directory
     #[clap(short, long, default_value("."))]
-    working_dir: String,
+    output_dir: String,
+    /// Specify the device to record from by index or by name
+    #[clap(short, long, default_value("default"))]
+    device: String,
     #[clap(subcommand)]
     command: Command,
 }
 
 #[derive(clap::Subcommand, Debug)]
 enum Command {
-    /// Interactively edit the configuration settings
+    /// Start interactive recording session
     Start {},
+    /// List the available devices
+    Devices {},
 }
 
 fn main() {
     let args = Args::parse();
-    match run(args) {
-        Err(e) => {
-            println!("{}", e);
-            std::process::exit(1)
-        }
-        Ok(msg) => {
-            println!("{}", msg);
-            std::process::exit(0)
-        }
+    match run_command(args) {
+        Err(e) => panic!("{}", e),
+        Ok(()) => std::process::exit(0),
     };
 }
 
-fn run(args: Args) -> Result<String> {
+fn run_command(args: Args) -> Result<()> {
     match args.command {
         Command::Start {} => {
-            record::main("default".to_string())?;
-            Ok("finished".to_string())
+            let device = resolve_device(&args.device);
+            record::run(&device)
         }
+        Command::Devices {} => list_devices(),
     }
+}
+
+pub fn resolve_output_dir(output_dir: &str) -> () {
+    if output_dir != "." {
+        env::set_current_dir(output_dir).expect("Could not access specified output directory.");
+    }
+}
+
+pub fn resolve_device(device_name: &str) -> Device {
+    let host = cpal::default_host();
+    if device_name == "default" {
+        host.default_input_device()
+    } else if let Ok(i) = device_name.parse::<usize>() {
+        // Find by index
+        host.input_devices().unwrap().nth(i)
+    } else {
+        // Find by name
+        host.input_devices()
+            .unwrap()
+            .find(|x| x.name().map(|y| y == device_name).unwrap_or(false))
+    }
+    .expect("Failed to find input device")
+}
+
+pub fn is_default_device(device_name: &str) -> bool {
+    let host = cpal::default_host();
+    match host.default_input_device().and_then(|dev| dev.name().ok()) {
+        Some(default_device_name) => device_name == default_device_name,
+        None => false,
+    }
+}
+
+pub fn list_devices() -> Result<()> {
+    let host = cpal::default_host();
+
+    host.input_devices()?.enumerate().for_each(|(i, dev)| {
+        match dev.name() {
+            Ok(name) => println!(
+                "{}: {} {}",
+                i,
+                &name,
+                if is_default_device(&name) { "(default)" } else { "" }
+            ),
+            Err(err) => println!("{}", err),
+        };
+    });
+
+    Ok(())
 }
--- src/record.rs
+++ src/record.rs
@@ -1,58 +1,208 @@
 //! Records a WAV file (roughly 3 seconds long) using the default input device and config.
 //!
 //! The input data is recorded to "$CARGO_MANIFEST_DIR/recorded.wav".
-
 extern crate anyhow;
 extern crate clap;
 extern crate cpal;
 extern crate hound;
 
+use cpal::Device;
+use crossterm::event::KeyEvent;
+use crossterm::{
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use regex::Regex;
+use tui::layout::Rect;
+use tui::widgets::{Block, Borders};
+use tui::{
+    backend::{Backend, CrosstermBackend},
+    layout::{Constraint, Direction, Layout},
+    style::{Color, Style},
+    text::{Span, Spans, Text},
+    widgets::Paragraph,
+    Frame, Terminal,
+};
+
 use anyhow::Result;
-use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+use cpal::traits::{DeviceTrait, StreamTrait};
+use std::cmp::max;
 use std::fs::File;
 use std::io::BufWriter;
 use std::sync::{Arc, Mutex};
 
-pub fn main(device: String) -> Result<()> {
-    let host = cpal::default_host();
+pub fn run(device: &Device) -> Result<()> {
+    // setup terminal
+    enable_raw_mode()?;
+    let mut stdout = std::io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
 
-    host.input_devices()?.for_each(|dev| {
-        match dev.name() {
-            Ok(name) => println!("{}", name),
-            Err(err) => println!("{}", err),
-        };
-    });
+    let mut messages: Vec<String> = vec![];
+    loop {
+        let filename = &determine_filename();
+        messages.push(format!("Started new recording {}", filename));
+        let action = main(&mut terminal, device, filename, &mut messages)?;
+        match action {
+            Action::StopAndQuit => break,
+            Action::CancelAndQuit => {
+                messages.push(format!("Deleting {}", filename));
+                std::fs::remove_file(filename).expect("Failed to remove recording");
+                break;
+            }
 
-    // Set up the input device and stream with the default input config.
-    let device = if device == "default" {
-        host.default_input_device()
-    } else {
-        host.input_devices()?
-            .find(|x| x.name().map(|y| y == device).unwrap_or(false))
+            Action::RestartRecording => {
+                messages.push(format!("Deleting {}", filename));
+                std::fs::remove_file(filename).expect("Failed to remove recording");
+            }
+            Action::StartAnotherRecording => {}
+        }
     }
-    .expect("failed to find input device");
 
-    println!("Input device: {}", device.name()?);
+    // restore terminal
+    disable_raw_mode()?;
+    execute!(terminal.backend_mut(), LeaveAlternateScreen, DisableMouseCapture)?;
+    terminal.show_cursor()?;
 
+    Ok(())
+}
+
+lazy_static! {
+    static ref RE: Regex = Regex::new("recording-(\\d+).wav").unwrap();
+}
+
+fn determine_filename() -> String {
+    let paths = std::fs::read_dir("./").unwrap();
+
+    let mut max_number: i32 = 1;
+
+    for path in paths {
+        let filename_os = path.unwrap().file_name();
+        let filename = filename_os.to_str().unwrap();
+        if let Some(capture) = RE.captures(filename) {
+            let number = capture[1].parse::<i32>().unwrap();
+            max_number = max(max_number, number);
+        }
+    }
+
+    format!("{}{}{}", "recording-", (max_number + 1).to_string(), ".wav")
+}
+
+fn run_app<B: Backend>(
+    terminal: &mut Terminal<B>,
+    filename: &str,
+    device_name: String,
+    messages: &Vec<String>,
+) -> Result<Action> {
+    loop {
+        terminal.draw(|f| ui(f, filename, &device_name, messages))?;
+
+        if let Event::Key(key) = event::read()? {
+            match get_action(key) {
+                Some(action) => return Ok(action),
+                None => {}
+            };
+        }
+    }
+}
+
+fn ui<B: Backend>(f: &mut Frame<B>, filename: &str, device_name: &str, messages: &Vec<String>) {
+    let chunks = Layout::default()
+        .direction(Direction::Vertical)
+        .margin(2)
+        .constraints([Constraint::Length(4), Constraint::Length(3), Constraint::Min(3)].as_ref())
+        .split(f.size());
+
+    render_help(f, chunks[0]);
+    render_status(f, chunks[1], filename, device_name);
+    render_messages(f, chunks[2], messages);
+}
+
+fn render_status<B: Backend>(f: &mut Frame<B>, rect: Rect, filename: &str, device_name: &str) {
+    let lines = vec![Spans::from(vec![
+        Span::raw("Recording from device "),
+        Span::styled(device_name, Style::default().fg(Color::Red)),
+        Span::raw(" to file "),
+        Span::styled(filename, Style::default().fg(Color::Red)),
+    ])];
+    f.render_widget(
+        Paragraph::new(Text::from(lines)).block(Block::default().title("STATUS").borders(Borders::ALL)),
+        rect,
+    );
+}
+
+fn render_help<B: Backend>(f: &mut Frame<B>, rect: Rect) {
+    let lines = vec![
+        Spans::from(vec![
+            Span::styled("Space", Style::default().fg(Color::Red)),
+            Span::raw(" to start a new recording"),
+        ]),
+        Spans::from(vec![
+            Span::styled("Backspace", Style::default().fg(Color::Red)),
+            Span::raw(" to start a new recording and delete the current recording\n"),
+        ]),
+        Spans::from(vec![
+            Span::styled("q", Style::default().fg(Color::Red)),
+            Span::raw(" to stop the recording and quit the application\n"),
+        ]),
+        Spans::from(vec![
+            Span::styled("Q", Style::default().fg(Color::Red)),
+            Span::raw(" to delete the current recording and quit the application\n"),
+        ]),
+    ];
+    f.render_widget(Paragraph::new(Text::from(lines)), rect);
+}
+
+fn render_messages<B: Backend>(f: &mut Frame<B>, rect: Rect, messages: &Vec<String>) {
+    let lines: Vec<Spans> = messages.iter().map(|msg| Spans::from(Span::raw(msg))).collect();
+    f.render_widget(
+        Paragraph::new(Text::from(lines)).block(Block::default().title("MESSAGES").borders(Borders::ALL)),
+        rect,
+    );
+}
+
+pub enum Action {
+    StopAndQuit,
+    CancelAndQuit, // Stop and delete current recording
+
+    RestartRecording, // Stop and delete current recording, and start a new recording
+    StartAnotherRecording,
+}
+
+fn get_action(key: KeyEvent) -> Option<Action> {
+    match key.code {
+        KeyCode::Char('q') => Some(Action::StopAndQuit),
+        KeyCode::Char('Q') => Some(Action::CancelAndQuit),
+
+        KeyCode::Char(' ') => Some(Action::StartAnotherRecording),
+        KeyCode::Backspace => Some(Action::RestartRecording),
+        _ => None,
+    }
+}
+
+pub fn main<B: Backend>(
+    terminal: &mut Terminal<B>,
+    device: &Device,
+    filename: &str,
+    messages: &mut Vec<String>,
+) -> Result<Action> {
+    // Set up the input device and stream with the default input config.
     let config = device
         .default_input_config()
         .expect("Failed to get default input config");
-    println!("Default input config: {:?}", config);
 
     // The WAV file we're recording to.
-    const PATH: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/recorded.wav");
     let spec = wav_spec_from_config(&config);
-    let writer = hound::WavWriter::create(PATH, spec)?;
+    let writer = hound::WavWriter::create(filename, spec)?;
     let writer = Arc::new(Mutex::new(Some(writer)));
-
-    // A flag to indicate that recording is in progress.
-    println!("Begin recording...");
 
     // Run the input stream on a separate thread.
     let writer_2 = writer.clone();
 
     let err_fn = move |err| {
-        eprintln!("an error occurred on stream: {}", err);
+        panic!("An error occurred on stream: {}", err);
     };
 
     let stream = match config.sample_format() {
@@ -75,12 +225,18 @@ pub fn main(device: String) -> Result<()> {
 
     stream.play()?;
 
-    // Let recording go for roughly three seconds.
-    std::thread::sleep(std::time::Duration::from_secs(3));
+    // Record until user gives an action
+    let action = run_app(
+        terminal,
+        filename,
+        device.name().unwrap_or("Unknown".to_string()),
+        messages,
+    );
+
     drop(stream);
     writer.lock().unwrap().take().unwrap().finalize()?;
-    println!("Recording {} complete!", PATH);
-    Ok(())
+
+    action
 }
 
 fn sample_format(format: cpal::SampleFormat) -> hound::SampleFormat {
